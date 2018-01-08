68e59949d0b1395fd2b4c662ebfe7ee42a8c85e2
saturn <saturn@glxl.net>
2018-01-08 02:33:57 +0000

    Remove stuff that prevents test server from starting up.

--- packages/lesswrong/lib/events/server.js
+++ packages/lesswrong/lib/events/server.js
@@ -1,11 +1,6 @@
 import { getSetting } from 'meteor/vulcan:lib';
-import Intercom from 'intercom-client';
-
-// Initiate Intercom on the server
-var intercomClient = new Intercom.Client({ token: getSetting("intercomToken") });
 
 
 import './callbacks_async.js';
 
 
-export default intercomClient
